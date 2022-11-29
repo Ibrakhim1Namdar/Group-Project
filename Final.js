d3c8d64bc6bc4c7d1f0676d937bde6079ec69e30
Ibrakhim1Namdar <210103381@stu.sdu.edu.kz>
2022-11-29 19:19:39 +0500

    today

--- Final.js
+++ Final.js
@@ -4,3 +4,6 @@ window.onscroll = function showHeader(){
         head.classList.add('header_fixed');
     }
 }
+function corusel(){
+        document.getElementById("car").style.backgroundImage = "url('https://ww1.prweb.com/prfiles/2017/01/10/13972471/APOK%20Townhome%20Rendering%20Boca%20Raton%20Florida.jpg')";
+}
