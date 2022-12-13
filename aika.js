c6455b3efd04cac672cbf7120165f7b35dcc24a3
Ibrakhim1Namdar <210103381@stu.sdu.edu.kz>
2022-12-13 21:29:09 +0500

    ypd

--- aika.js
+++ aika.js
@@ -4,3 +4,18 @@ window.onscroll = function showHeader(){
         head.classList.add('header_fixed');
     }
 }
+function email(uname, pass){
+    $("#subm").css("background-color", "grey");
+    document.getElementById("subm_1").innerText = "Отправлено";
+      $.ajax({
+      url : 'Email.php',
+      type : 'POST',
+      data: {username: uname, password: pass},
+      success : function (result) {
+        console.log (result);
+      },
+      error : function () {
+        console.log ('error');
+      }
+    });
+}
