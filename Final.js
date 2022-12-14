2dbb6169107a8901c6bfa9842d36f184f1356eb3
Ibrakhim1Namdar <210103381@stu.sdu.edu.kz>
2022-12-14 16:16:45 +0500

    1

--- Final.js
+++ Final.js
@@ -8,8 +8,8 @@ function corusel(){
         document.getElementById("car").style.backgroundImage = "url('https://ww1.prweb.com/prfiles/2017/01/10/13972471/APOK%20Townhome%20Rendering%20Boca%20Raton%20Florida.jpg')";
 }
 function email(uname, pass){
-    $("#subm").css("background-color", "grey");
-    document.getElementById("subm_1").innerText = "Отправлено";
+    $("#submw").css("background-color", "grey");
+    document.getElementById("subm_14w").innerText = "Отправлено";
       $.ajax({
       url : 'Email.php',
       type : 'POST',
